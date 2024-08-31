050ef6f82b62e2678dec366073b2ce744f7d4c38
siwikrt <siwikartikadewi@gmail.com>
2024-08-31 21:53:54 +0700

    menambahkan foler src dan target

---
+++ Documents/NetBeansProjects/siwi_project1/src/main/java/com/mycompany/siwi_project1/Siwi_project1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.siwi_project1;
+
+/**
+ *
+ * @author User
+ */
+public class Siwi_project1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Nama     : Siwi Kartika Dewi");
+        System.out.println("Alamat   : Magelang");
+        System.out.println("No HP    : 085612345678");
+    }
+}
